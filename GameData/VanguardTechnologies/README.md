82ceb432c2c9545c8d66ef44025ba9c6e2ae6b26
Jonathan Bayer <linuxgurugamer@gmail.com>
2017-05-30 11:35:53 -0400

    Updated for 1.3

---
+++ GameData/VanguardTechnologies/README.md
@@ -0,0 +1,43 @@
+This mod provides parachutes for all Kerbals on EVA, and ejection modules, 
+to be able to eject all kerbals from a craft in an Abort sequence
+
+There are two parts for the ejection, a 1 person and an 3 person module.  
+Obviously, the 1 person module can eject a single Kerbal, and the 3 person
+can eject up to 3 Kerbals.
+
+To use, place the ejection module on the crew cabin desired.  If you have
+more crew than the module can handle, you can place multiple modules in order
+to be able to eject all the crew.
+
+You can change which parachute is used by right-clicking on the ejection module
+and selecting the desired chute.  At this time, there is no difference in performance,
+it is merely a matter of display preference
+
+Weight and cost for each ejection module is calculated based on whichever is lower of
+the following:
+	The size of the ejection module
+	The MAX crew that the crew pod can support.
+
+Research Supersonic flight for the 1 person Ejection Module
+Research High Altitude Flight for the 3 person Ejection Module
+
+-The ejection module activates with the abort action group (backspace or 
+ mouseover the altimeter, then the red button to the left), but you can of 
+ course put it into any other action group, too
+
+-To deploy chutes, either use the Kerbal's context menu or press Jump and 
+ Use (Space+F). Jump+Use has the advantage of opening chutes on all Kerbals 
+ in range (good for mass ejects), but you may have to hold it down for a 
+ short time or it won't work (not sure why)
+
+ This is a revivial of the old EVA Parachutes.
+ New models have been provided with new art provided by "SpannerMonkey(smce)"
+ Old square chute is still included, permission granted by "Kreuzung"
+
+ There is a MM patch file included for those who would like to add the ejection 
+ module to all manned pods without the need for the ejection parts.  The file
+ is called:
+		addEjectionToAll.cfg.patch
+
+To enable this, rename the file to:
+		addEjectionToAll.cfg
--- Source/VanguardTechnologies/buildRelease.bat
+++ Source/VanguardTechnologies/buildRelease.bat
@@ -1,31 +1,8 @@
 ﻿
 @echo off
-set DEFHOMEDRIVE=d:
-set DEFHOMEDIR=%DEFHOMEDRIVE%%HOMEPATH%
-set HOMEDIR=
-set HOMEDRIVE=%CD:~0,2%
 
 set RELEASEDIR=d:\Users\jbb\release
 set ZIP="c:\Program Files\7-zip\7z.exe"
-echo Default homedir: %DEFHOMEDIR%
-
-rem set /p HOMEDIR= "Enter Home directory, or <CR> for default: "
-
-if "%HOMEDIR%" == "" (
-set HOMEDIR=%DEFHOMEDIR%
-)
-echo %HOMEDIR%
-
-SET _test=%HOMEDIR:~1,1%
-if "%_test%" == ":" (
-set HOMEDRIVE=%HOMEDIR:~0,2%
-)
-
-
-rem type EVAParachutesAndEjectionSeats.version
-rem set /p VERSION= "Enter version: "
-
-
 
 copy EVAParachutesAndEjectionSeats.version a.version
 set VERSIONFILE=a.version
@@ -48,31 +25,27 @@ if "%build%" NEQ "0"  set VERSION=%VERSION%.%build%
 
 echo %VERSION%
 
+set HOMEDIR=D:\Users\jbb\github\EVAParachutesAndEjectionSeats
+mkdir %HOMEDIR%\GameData\VanguardTechnologies
+mkdir %HOMEDIR%\GameData\VanguardTechnologies\Plugins
+mkdir %HOMEDIR%\GameData\VanguardTechnologies\Parts
+mkdir %HOMEDIR%\GameData\VanguardTechnologies\Sounds
 
-rd /s %HOMEDIR%\install\GameData\VanguardTechnologies
-mkdir %HOMEDIR%\install\GameData\VanguardTechnologies
-mkdir %HOMEDIR%\install\GameData\VanguardTechnologies\Plugins
-mkdir %HOMEDIR%\install\GameData\VanguardTechnologies\Parts
-mkdir %HOMEDIR%\install\GameData\VanguardTechnologies\Sounds
+copy /Y "%~dp0bin\Release\VanguardTechnologies.dll"  %HOMEDIR%\GameData\VanguardTechnologies\Plugins
+xcopy /y /s "%~dp0..\..\GameData\VNG\Parts" "%HOMEDIR%\GameData\VanguardTechnologies\Parts"
+copy /y ..\..\GameData\VNG\addEjectionToAll.cfg.patch "%HOMEDIR%\GameData\VanguardTechnologies"
+copy /y ..\..\GameData\VNG\FAR_patch.cfg "%HOMEDIR%\GameData\VanguardTechnologies"
 
-copy /Y "%~dp0bin\Release\VanguardTechnologies.dll"  %HOMEDIR%\install\GameData\VanguardTechnologies\Plugins
-xcopy /y /s "%~dp0..\..\GameData\VNG\Parts" "%HOMEDIR%\install\GameData\VanguardTechnologies\Parts"
-copy /y ..\..\GameData\VNG\addEjectionToAll.cfg.patch "%HOMEDIR%\install\GameData\VanguardTechnologies"
-copy /y ..\..\GameData\VNG\FAR_patch.cfg "%HOMEDIR%\install\GameData\VanguardTechnologies"
+xcopy /y /s "%~dp0..\..\GameData\VNG\Sounds" "%HOMEDIR%\GameData\VanguardTechnologies\Sounds"
 
-xcopy /y /s "%~dp0..\..\GameData\VNG\Sounds" "%HOMEDIR%\install\GameData\VanguardTechnologies\Sounds"
-
-rem rd /Q /s "%HOMEDIR%\install\GameData\VanguardTechnologies\Parts\VNG_Eject"
-
-copy /Y "EVAParachutesAndEjectionSeats.version" "%HOMEDIR%\install\GameData\VanguardTechnologies"
+copy /Y "EVAParachutesAndEjectionSeats.version" "%HOMEDIR%\GameData\VanguardTechnologies"
 
 
-copy /Y "..\..\License.txt" "%HOMEDIR%\install\GameData\VanguardTechnologies"
-copy /Y "README.md" "%HOMEDIR%\install\GameData\VanguardTechnologies"
-copy /Y MiniAVC.dll  "%HOMEDIR%\install\GameData\VanguardTechnologies"
+copy /Y "..\..\License.txt" "%HOMEDIR%\GameData\VanguardTechnologies"
+copy /Y "README.md" "%HOMEDIR%\GameData\VanguardTechnologies"
+copy /Y MiniAVC.dll  "%HOMEDIR%\GameData\VanguardTechnologies"
 
-%HOMEDRIVE%
-cd %HOMEDIR%\install
+cd %HOMEDIR%
 
 set FILE="%RELEASEDIR%\VanguardTechnologies-%VERSION%.zip"
 IF EXIST %FILE% del /F %FILE%
